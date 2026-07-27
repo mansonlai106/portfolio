29b5a8cb2a05141faa02d434c321d0d13312d285
Manson <mansonlai106@gmail.com>
2026-07-27 14:13:35 +0800

    feat: update hero title

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
         </p>
 
         <h1 className="text-5xl font-bold tracking-tight sm:text-7xl">
-          Hi, I&apos;m Manson.
+          Hi, I&apos;m Manson. Nice to meet you.
         </h1>
 
         <p className="max-w-2xl text-lg leading-8 text-zinc-300 sm:text-xl">
